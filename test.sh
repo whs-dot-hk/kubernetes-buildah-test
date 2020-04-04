0db8e6ad5586a601d1d0ba92486f4fc32925330d
whs <hswongac@gmail.com>
2020-04-04 10:59:49 +0800

    Add index-dot-html

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@ ctr=$(buildah from docker.io/nginx)
 
 mnt=$(buildah mount $ctr)
 
+echo "Hello world" > index.html
+cp index.html $mnt/usr/share/nginx/html
+
 buildah unmount $ctr
 
 buildah commit $ctr test
